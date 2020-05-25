a14a821e4396181154bf5d5d8f10082a67f7b9f4
parpassorn02 <parpassorn02nokki@gmail.com>
2020-05-25 22:50:26 +0200

    Updates

---
+++ precache-manifest.7aea4b1ecda134e580c9f79370526e6f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa470d736ed47116b6bb119c0b9fd590",
+    "url": "/first-project/index.html"
+  },
+  {
+    "revision": "688f3b19529521699f4c",
+    "url": "/first-project/static/css/main.1efd811d.chunk.css"
+  },
+  {
+    "revision": "81ff53412b5a24f0438d",
+    "url": "/first-project/static/js/2.716722f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first-project/static/js/2.716722f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "688f3b19529521699f4c",
+    "url": "/first-project/static/js/main.53307b54.chunk.js"
+  },
+  {
+    "revision": "205e58f331a0d1779041",
+    "url": "/first-project/static/js/runtime-main.001afe9d.js"
+  },
+  {
+    "revision": "bd3513f91061f0a4e652e98a06c500a5",
+    "url": "/first-project/static/media/Pad-Thai-Recipe-1.bd3513f9.jpg"
+  },
+  {
+    "revision": "a3662c77f52025a4e31c5f2371149c24",
+    "url": "/first-project/static/media/Som tam.a3662c77.jpg"
+  },
+  {
+    "revision": "927a87a6f6b24eeae5c0d9284c345270",
+    "url": "/first-project/static/media/banana pancake.927a87a6.webp"
+  },
+  {
+    "revision": "d9fdc6c6249bb40022b76ea3c0b0d358",
+    "url": "/first-project/static/media/food_pic.d9fdc6c6.jpg"
+  },
+  {
+    "revision": "17a116bac6ba81efde5721576c45475d",
+    "url": "/first-project/static/media/gai tod.17a116ba.webp"
+  },
+  {
+    "revision": "9549905a3f20ea6429f14ab5ac593943",
+    "url": "/first-project/static/media/khao neeo mamuang.9549905a.jpg"
+  },
+  {
+    "revision": "0bb8f0a7588bf1203767e52e0da0392f",
+    "url": "/first-project/static/media/moo ping.0bb8f0a7.jpg"
+  },
+  {
+    "revision": "12e279b3620a10a642696fd4f1f1b723",
+    "url": "/first-project/static/media/pad krapow.12e279b3.jpg"
+  },
+  {
+    "revision": "13926114edf06d1f8bf0306c59548f34",
+    "url": "/first-project/static/media/pad see ew.13926114.jpg"
+  },
+  {
+    "revision": "6bce71e5f412b1a4d223d0d73621c04a",
+    "url": "/first-project/static/media/panang-curry.6bce71e5.png"
+  },
+  {
+    "revision": "dec1c581bed050c92ffe9e7fb724616f",
+    "url": "/first-project/static/media/thai red curry.dec1c581.jpg"
+  },
+  {
+    "revision": "b3f8d8a44342d47b212d36081f6e3711",
+    "url": "/first-project/static/media/thai-fried-rice.b3f8d8a4.jpg"
+  },
+  {
+    "revision": "56892461b091786e1627c418ba1527e7",
+    "url": "/first-project/static/media/yum woon sen.56892461.jpg"
+  }
+]);
